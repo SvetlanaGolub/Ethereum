a98149db28f8fa3d4e50671716de7630614641c9
Svetlana <sveta.golub@yandex.ru>
2022-12-02 13:49:44 -0500

    lab3

---
+++ lab3/README.md
@@ -0,0 +1,2 @@
+Ссылка на контракт в Etherscan:   
+https://sepolia.etherscan.io/address/0x2fa1a2c32c5707125757c6538c4ab2d1a96ea9f3
---
+++ lab3/hardhat.config.js
@@ -0,0 +1,17 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers")
+require("@nomicfoundation/hardhat-chai-matchers")
+require("dotenv").config();
+const { API_URL, PRIVATE_KEY } = process.env;
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  solidity: "0.8.17",
+};
